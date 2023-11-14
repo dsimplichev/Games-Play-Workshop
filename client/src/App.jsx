8af44ab80c9a60f1e9f864393845b96af6e0ded9
dsimplichev <dsimplichev1948@gmail.com>
2023-11-14 14:50:44 +0000

    add home and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,7 @@
-import Header from "./components/header/Header"
+import { Routes, Route } from 'react-router-dom'
 
+import Header from "./components/header/Header"
+import Home from "./components/header/home/Home"
 
 function App() {
 
@@ -7,6 +9,12 @@ function App() {
   return (
     <div id="box">
       <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+
+
     </div>
   )
 }

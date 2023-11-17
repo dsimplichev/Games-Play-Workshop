b73beb4881cacd8279226249931d33b489bf38b9
dsimplichev <dsimplichev1948@gmail.com>
2023-11-17 17:14:02 +0000

    add get all games and requester

--- client/src/components/game-list/GameList.jsx
+++ client/src/components/game-list/GameList.jsx
@@ -4,44 +4,49 @@ import * as gameService from '../../services/gameService';
 
 
 export default function GameList() {
-   useEffect(() => {
+    const [games, setGames] = useState([]);
 
-   }, []);
-   
-   
-    return(
+    useEffect(() => {
+        gameService.getAll()
+            .then(result => setGames(result));
+
+    }, []);
+ 
+    console.log(games)
+
+    return (
         <section id="catalog-page">
-        <h1>All Games</h1>
-        
-        <div className="allGames">
-            <div className="allGames-info">
-                <img src="./images/avatar-1.jpg" />
-                <h6>Action</h6>
-                <h2>Cover Fire</h2>
-                <a href="#" className="details-button">Details</a>
+            <h1>All Games</h1>
+
+            <div className="allGames">
+                <div className="allGames-info">
+                    <img src="./images/avatar-1.jpg" />
+                    <h6>Action</h6>
+                    <h2>Cover Fire</h2>
+                    <a href="#" className="details-button">Details</a>
+                </div>
+
+            </div>
+            <div className="allGames">
+                <div className="allGames-info">
+                    <img src="./images/avatar-1.jpg" />
+                    <h6>Action</h6>
+                    <h2>Zombie lang</h2>
+                    <a href="#" className="details-button">Details</a>
+                </div>
+
+            </div>
+            <div className="allGames">
+                <div className="allGames-info">
+                    <img src="./images/avatar-1.jpg" />
+                    <h6>Action</h6>
+                    <h2>MineCraft</h2>
+                    <a href="#" className="details-button">Details</a>
+                </div>
             </div>
 
-        </div>
-        <div className="allGames">
-            <div className="allGames-info">
-                <img src="./images/avatar-1.jpg" />
-                <h6>Action</h6>
-                <h2>Zombie lang</h2>
-                <a href="#" className="details-button">Details</a>
-            </div>
 
-        </div>
-        <div className="allGames">
-            <div className="allGames-info">
-                <img src="./images/avatar-1.jpg" />
-                <h6>Action</h6>
-                <h2>MineCraft</h2>
-                <a href="#" className="details-button">Details</a>
-            </div>
-        </div>
-
-        
-        <h3 className="no-articles">No articles yet</h3>
-    </section>
+            <h3 className="no-articles">No articles yet</h3>
+        </section>
     )
 }

d257650b3d131959c5f5f18cb15dc979d42a87ae
dsimplichev <dsimplichev1948@gmail.com>
2023-11-18 11:34:15 +0000

    new changes on game create and details

--- client/src/components/game-create/GameCreate.jsx
+++ client/src/components/game-create/GameCreate.jsx
@@ -1,33 +1,29 @@
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
 
-import * as gameService  from "../../services/gameService";
+import * as gameService from '../../services/gameService';
 
 export default function GameCreate() {
-    const navigate = useNavigate()
+    const navigate = useNavigate();
     
     const createGameSubmitHandler = async (e) => {
         e.preventDefault();
-        const gameData = Object.fromEntries(new FormData(e.currentTarget))
-       
+
+        const gameData = Object.fromEntries(new FormData(e.currentTarget));
+
         try {
-            await gameService.create(gameData)
+            await gameService.create(gameData);
+
             navigate('/games');
         } catch (err) {
+            // Error notification
             console.log(err);
         }
-        
-        
-
-      
     }
 
-
-
-    return(
+    return (
         <section id="create-page" className="auth">
             <form id="create" onSubmit={createGameSubmitHandler}>
                 <div className="container">
-
                     <h1>Create Game</h1>
                     <label htmlFor="leg-title">Legendary title:</label>
                     <input type="text" id="title" name="title" placeholder="Enter game title..." />
@@ -47,5 +43,5 @@ export default function GameCreate() {
                 </div>
             </form>
         </section>
-    )
+    );
 }

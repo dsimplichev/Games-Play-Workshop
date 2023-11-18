1e593c5021936e316394654c52caafe7035aeb4c
dsimplichev <dsimplichev1948@gmail.com>
2023-11-18 11:38:53 +0000

    app from 0

--- client/src/services/gameService.js
+++ client/src/services/gameService.js
@@ -1,25 +1,25 @@
-import * as request from "../lib/request";
+import * as request from '../lib/request';
 
-const baseUrl = 'http://localhost:3030/jsonstore/games'
+const baseUrl = 'http://localhost:3030/jsonstore/comments';
 
-export const getAll = async () => {
- const result = await request.get(baseUrl);
+export const getAll = async (gameId) => {
+    const query = new URLSearchParams({
+        where: `gameId="${gameId}"`
+    });
 
- return Object.values(result)
+    const result = await request.get(`${baseUrl}`);
+
+    // TODO: temp solution until migration to collections service 
+    return Object.values(result).filter(comment => comment.gameId === gameId);
 };
 
-export const getOne = async (gameId) => {
-    const result = await request.get(`${baseUrl}/${gameId}`)
+export const create = async (gameId, username, text) => {
+    const newComment = await request.post(baseUrl, {
+        gameId,
+        username, 
+        text,
+    });
 
-   
-    return result
-
-}
-export const create = async (gameData) => {
-    const result = await request.post(baseUrl, gameData )
-
-
-    return result;
-}
-
+    return newComment;
+};
 

7935acfbbfd5b31e037124954f575fd2b47b6ced
dsimplichev <dsimplichev1948@gmail.com>
2023-11-21 15:15:53 +0000

    working on login

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,4 +1,17 @@
-export default function useForm() {
+import { useState } from "react"
 
+export default function useForm(initialValues) {
+    const [values, setValues] = useState(initialValues);
+
+    const onChange = (e) => {
+        setValues(state => ({
+            ...state,
+           [e.target.name]: e.target.value
+        }))
+};
+    return {
+       values,
+       onChange,
+    }
 
 }
